184f2ffad3f886569afbe4b70aac7f6bfa4a9f77
parvizrovshanaliyev <parvizra@code.edu.az>
2019-05-05 18:00:53 +0400

    userapp model

---
+++ AsusLaptop/App_Start/OwinStartup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AsusLaptop.App_Start.OwinStartup))]
+
+namespace AsusLaptop.App_Start
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
---
+++ AsusLaptop/Models/UserApp.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AsusLaptop.Models
+{
+    public class UserApp:IdentityUser
+    {
+        public bool Status { get; set; }
+
+
+        [StringLength(50)]
+        public string Fullname { get; set; }
+
+        public DateTime? CreateAt { get; set; }
+    }
+}
