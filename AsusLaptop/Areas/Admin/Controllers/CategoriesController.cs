d62bf07577e2a770e0e00699a0fd470bbc582624
parvizrovshanaliyev <parvizra@code.edu.az>
2019-05-11 17:36:25 +0400

    categories view ready

---
+++ AsusLaptop/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using AsusLaptop.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsusLaptop.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly AsusDbContext _context;
+
+        public CategoriesController()
+        {
+            _context = new AsusDbContext();
+        }
+        // GET: Admin/Categories
+        public ActionResult Index()
+        {
+            return View(_context.Categories.ToList());
+        }
+    }
+}
--- AsusLaptop/Migrations/Configuration.cs
+++ AsusLaptop/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 namespace AsusLaptop.Migrations
 {
+    using AsusLaptop.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web.Helpers;
 
     internal sealed class Configuration : DbMigrationsConfiguration<AsusLaptop.DAL.AsusDbContext>
     {
@@ -14,6 +16,26 @@ namespace AsusLaptop.Migrations
 
         protected override void Seed(AsusLaptop.DAL.AsusDbContext context)
         {
+
+            context.Roles.AddOrUpdate(r => new { r.Name }, new RoleApp { Name = "admin" });
+
+            context.Users.AddOrUpdate(u => new { u.Email, u.UserName },
+
+               new Models.UserApp
+               {
+
+                   Email = "parviz@gmail.com",
+                   UserName = "parvizra",
+                   PasswordHash = Crypto.HashPassword("parviz123"),
+                   SecurityStamp = Crypto.Hash(DateTime.Now.ToString("yyyyMMddHHmmssfff")),
+                   
+               });
+
+            context.AspNetUserRoles.Add(new AspNetUserRole
+            {
+                UserId = "b9d3a126-e235-415c-9248-bc242b9ab992",
+                RoleId = "fb7450e2-5b29-4828-851d-547d38ed19c6",
+            });
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
