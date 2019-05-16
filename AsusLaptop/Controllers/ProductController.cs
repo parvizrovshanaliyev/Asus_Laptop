235ea7c579c1b547303c2027fc98782f7a54242d
parvizrovshanaliyev <parvizra@code.edu.az>
2019-05-16 19:46:45 +0400

    product details ready

---
+++ AsusLaptop/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using AsusLaptop.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsusLaptop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AsusDbContext _context;
+
+        public ProductController()
+        {
+            _context = new AsusDbContext();
+        }
+        // GET: ProductDetails
+        [Route("Product/{title}/{id}")]
+        public ActionResult Detail(string title,int id)
+        {
+            var product = _context.Products.Where(p => p.Id == id).FirstOrDefault();
+            return View(product);
+        }
+    }
+}
