5f57f4022760f54e8886ec4c15843a60a8533015
parvizrovshanaliyev <parvizra@code.edu.az>
2019-05-17 20:42:53 +0400

    blog-details

---
+++ AsusLaptop/Controllers/BlogDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsusLaptop.Controllers
+{
+    public class BlogDetailsController : Controller
+    {
+        // GET: BlogDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
