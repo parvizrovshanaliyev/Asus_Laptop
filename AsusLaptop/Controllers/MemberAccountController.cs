a29d66e002b6da9816a4998c9e877694fd0718f4
parvizrovshanaliyev <parvizra@code.edu.az>
2019-05-20 08:38:50 +0400

    login view and model ready

---
+++ AsusLaptop/Controllers/MemberAccountController.cs
@@ -0,0 +1,51 @@
+﻿using AsusLaptop.Areas.Admin.Models;
+using AsusLaptop.DAL;
+using AsusLaptop.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsusLaptop.Controllers
+{
+    public class MemberAccountController : Controller
+    {
+        private readonly AsusDbContext _context;
+        public MemberAccountController()
+        {
+            _context = new AsusDbContext();
+        }
+
+        public UserManagerApp UserManagerApp
+        {
+            get
+            {
+                IOwinContext context = HttpContext.GetOwinContext();
+
+                return context.GetUserManager<UserManagerApp>();
+            }
+        }
+
+        public RoleManagerApp RoleManagerApp { get { return HttpContext.GetOwinContext().GetUserManager<RoleManagerApp>(); } }
+
+
+        [AllowAnonymous]
+        // GET: MemberAccount
+        public ActionResult Login(string returnURL)
+        {
+
+            ViewBag.url = returnURL;
+            return View();
+        }
+    }
+}
---
+++ AsusLaptop/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AsusLaptop.Models
+{
+    public class Login
+    {
+        [StringLength(100), EmailAddress, Required]
+        public string Email { get; set; }
+
+        [StringLength(100), Required, MinLength(6)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remmember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
