7c6ee67ebd91487308902cd532d9bba56568e59e
parvizrovshanaliyev <parvizra@code.edu.az>
2019-05-20 18:47:07 +0400

    compre html added

---
+++ AsusLaptop/Controllers/CompareController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsusLaptop.Controllers
+{
+    public class CompareController : Controller
+    {
+        // GET: Compare
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
