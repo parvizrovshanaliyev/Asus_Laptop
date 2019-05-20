790bcf215aee6641ec4a687b517675864304e945
parvizrovshanaliyev <parvizra@code.edu.az>
2019-05-20 23:34:30 +0400

    blog page her seyi tamamdi

--- AsusLaptop/Controllers/BlogController.cs
+++ AsusLaptop/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AsusLaptop.DAL;
+using AsusLaptop.Models.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,21 @@ namespace AsusLaptop.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly AsusDbContext _context;
+
+        public BlogController()
+        {
+            _context = new AsusDbContext();
+        }
         // GET: Blog
         public ActionResult Index()
         {
-            return View();
+            var vm = new BlogVM()
+            {
+               Blogs=_context.Blogs.Where(p => p.Status == true).OrderByDescending(b => b.CreateAt).ToList(),
+               BlogDesc = _context.Blogs.Where(p => p.Status == true).OrderByDescending(b => b.CreateAt).Take(3).ToList()
+            };
+            return View(vm);
         }
     }
 }
